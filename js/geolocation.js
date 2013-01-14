da3c11a23d655d5e803d050ac2d2f529457b57df
Simon Blum <simon51100@gmail.com>
2013-01-14 13:40:30 +0100

    Big mis à jour

---
+++ js/geolocation.js
@@ -0,0 +1,73 @@
+var parcours_id=null;
+var cpt=0;
+var watchId=null;
+
+var start_gps=function(parcours){
+
+
+
+	parcours_id=parcours;
+	watchId = navigator.geolocation.watchPosition(successCallback, errorCallback, {enableHighAccuracy:true});
+	
+	
+}
+
+
+
+function successCallback(position){
+		
+  if(cpt==0){
+	  
+	  var newobj={};
+	  newobj.LatStartPoint=position.coords.latitude;
+	  newobj.LongStartPoint=position.coords.longitude;
+	  newobj.startTime=new Date().getTime();
+	 console.log(parcours_id);
+	 
+	 get_parcours_and_begin(parcours_id,newobj);
+	 
+	  //get_parcours(parcours_id,update_parcours(parcours_id,newobj));
+	 
+  }
+  
+  var obj = {};
+  obj.latitude=position.coords.latitude;
+  obj.longitude=position.coords.longitude;
+  obj.timestamp=position.timestamp;
+  obj.altitude=position.coords.altitude;
+  obj.accuracy=position.coords.accuracy;
+  obj.altitudeAccuracy=position.coords.altitudeAccuracy;
+  obj.heading=position.coords.heading;
+  obj.speed=position.coords.speed;
+  obj.parcours_id=parcours_id;
+  
+  add_record(obj)
+cpt++;
+};  
+ 
+function errorCallback(error){
+  switch(error.code){
+    case error.PERMISSION_DENIED:
+      console.log("L'utilisateur n'a pas autorisé l'accès à sa position");
+      break;      
+    case error.POSITION_UNAVAILABLE:
+       console.log("L'emplacement de l'utilisateur n'a pas pu être déterminé");
+      break;
+    case error.TIMEOUT:
+       console.log("Le service n'a pas répondu à temps");
+      break;
+    }
+};
+ 
+  function stopWatch(){
+  
+    var newobj={};
+	  newobj.StopTime=new Date().getTime();	  
+	  
+	 
+  get_parcours_and_finish(parcours_id,newobj);
+  
+  navigator.geolocation.clearWatch(watchId);
+  } 
+
+
---
+++ js/gmaps.js
@@ -0,0 +1,73 @@
+function toRad(Value) {
+		    /** Converts numeric degrees to radians */
+		    return Value * Math.PI / 180;
+}
+		
+		
+function CalcDistanceBetween(lat1, lon1, lat2, lon2) {
+		    var R = 6378.1; // Radius of earth in Km var R = (6371 / 1.609344);
+		    var dLat = toRad(lat2-lat1);
+		    var dLon = toRad(lon2-lon1); 
+		    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * 
+		            Math.sin(dLon/2) * Math.sin(dLon/2); 
+		    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+		    var d = R * c;
+		    return d;
+}
+	     
+	     
+	     
+	      function polyline() {
+		    
+		    var path_polyline = new Array();
+			var my_distance = 0;
+			for(var i = 0;i<dataLat.length;i++) {
+				if(i+1 != dataLat.length) {
+					my_distance = my_distance + CalcDistanceBetween(dataLat[i], dataLong[i], dataLat[i+1], dataLong[i+1]);
+				}
+				var p = new google.maps.LatLng(dataLat[i], dataLong[i]);
+				path_polyline.push(p);
+			}
+		
+			document.getElementById("distance").innerHTML= my_distance;
+			console.log(my_distance);
+			
+			
+	        var line = new google.maps.Polyline({
+	          path: path_polyline,
+	          strokeColor: '#ff0000',
+	          strokeOpacity: 1.0,
+	          strokeWeight: 3
+	        });
+	        line.setMap(map);
+			
+			
+			
+
+	     }
+	     
+	     
+	     
+	     function initialize() {
+    var latlng = new google.maps.LatLng(48.725623,2.272943);
+    var myOptions = {
+        zoom: 12,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+   map = new google.maps.Map(document.getElementById("map"),
+            myOptions);
+      
+      
+}
+ 
+//en haut à gauche 48.758226,2.199986
+//en bas à gauche 48.702857,2.206166
+//en bas à droite 48.702064,2.325643
+//en haut à droite : 48.755397,2.320493
+
+
+  google.maps.event.addDomListener(window, "load", initialize);
+	     
+	     
